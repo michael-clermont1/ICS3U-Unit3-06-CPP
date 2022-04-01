8c9d5bdeda5e461359c0f60f6daed62cad200b3b
Michael Clermont <michael.clermont@mths.ca>
2022-04-01 00:47:48 +0000

    added guessing game program

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -5,16 +5,18 @@
 // This program is a guessing game
 
 #include <iostream>
+#include <string>
 #include <random>
 
 int main() {
     // this function is a guessing game
-    int numberFromUser;
+    std::string integerAsString;
+    int integerAsNumber;
     int randomNumber;
 
     // input
     std::cout << "Enter a number between 0-9: ";
-    std::cin >> numberFromUser;
+    std::cin >> integerAsString;
     std::random_device rseed;
     std::mt19937 rgen(rseed());
     std::uniform_int_distribution<int> idist(0, 9);
@@ -22,10 +24,15 @@ int main() {
     std::cout << "" << std::endl;
 
     // process & output
-    if (numberFromUser == randomNumber) {
+    try {
+        integerAsNumber = std::stoi(integerAsString);
+        if (integerAsNumber == randomNumber) {
         std::cout << "You guessed it correctly!";
-    } else {
+        } else {
         std::cout << "You guessed it incorrectly!";
-    }
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "That is not an integer.";
+        }
     std::cout << "\nDone." << std::endl;
 }
